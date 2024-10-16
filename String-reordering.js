52af8dcbe19fc38031c596f245cc0eeaa8f60cea
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-15 19:42:45 -0700

    Update String-reordering.js

--- String-reordering.js
+++ String-reordering.js
@@ -20,3 +20,18 @@ Example:
 
 
 // Solution
+
+function sentence(List) {
+  let arr = [];
+  let res = '';
+  let min = 0;
+   for (let key of List) {
+     arr.push(+Object.keys(key))
+   }
+  for (let j of arr) {
+    min = Math.min(...arr);
+    res += Object.values(List[arr.indexOf(min)]) + ' ';
+    arr.splice(arr.indexOf(min), 1, 999);  
+  }
+  return res.trim();
+}
