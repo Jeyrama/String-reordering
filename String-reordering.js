2bada34c066a4a0ad95d1f166cfbfc073fe7abda
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-15 19:43:06 -0700

    Update String-reordering.js

--- String-reordering.js
+++ String-reordering.js
@@ -37,3 +37,9 @@ function sentence(List) {
 }
 
 // or
+
+const sentence = list =>
+  list
+    .sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
+    .map(item => Object.values(item)[0])
+    .join(' ')
